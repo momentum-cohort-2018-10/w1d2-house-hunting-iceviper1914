c4f21e46db5c94aa5c5fd3e4d3e6684a63952d80
iceviper1914 <mehiorob@gmail.com>
2018-10-30 13:39:39 -0400

    Adding House Hunting Assingment

---
+++ house_hunting.py
@@ -0,0 +1,4 @@
+salary = float(input("What is your starting annual salary? "))
+portion_saved = float(input("What is the portion that you have saved? "))
+cost_of_dream_home = float(input("What is the cost of your dream home? "))
+
