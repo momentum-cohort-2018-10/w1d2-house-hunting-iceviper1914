b6c49646627eb96ad9b1c4b71711a9d30db7249d
iceviper1914 <mehiorob@gmail.com>
2018-10-31 09:36:08 -0400

    Finsihed Homework

--- house_hunting.py
+++ house_hunting.py
@@ -1,4 +1,21 @@
 salary = float(input("What is your starting annual salary? "))
 portion_saved = float(input("What is the portion that you have saved? "))
 cost_of_dream_home = float(input("What is the cost of your dream home? "))
+print("What is your starting annual salary? ", salary, "What is the portion that you have save? ", portion_saved, "What is the cost of your dream home? ", cost_of_dream_home)
+
+r = 0.04
+portion_down_payment = 0.25 
+actual_down_payment = (portion_down_payment * cost_of_dream_home)
+current_savings = 0
+montly_income = (salary /12) * (portion_saved)
+mon_rot = (r /12)
+month = 0
+
+
+while current_savings < actual_down_payment :
+    month += 1
+    current_savings += (mon_rot * current_savings) + montly_income
+    
+    print("Number of months is ", month)
+
 
